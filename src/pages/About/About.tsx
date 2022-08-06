ecca82ff7947037de88bea7860e8bdf3bada0703
Artyom <sheludeshev.artyom@mail.ru>
2022-08-06 19:39:41 +0300

    Connected sass variables. Filled fully About page with html content

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -60,10 +60,136 @@ export default function About() {
           </div>
         </section>
 
-        <section>
-          
+        <section className={styles.section}>
+          <h3 className={styles.section_heading}>My Skills</h3>
+          <div className={styles.section_content}>
+            <div className={styles.skills}>
+              <div className={styles.skill}>
+                <figure className={styles.skill_image}>
+                  ...here will be image
+                </figure>
+                <h4 className={styles.skill_heading}>Frontend</h4>
+                <ul className={styles.skill_list}>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>React</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Redux toolkit</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Tailwind CSS</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Next.js</span>
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.skill}>
+                <figure className={styles.skill_image}>
+                  ...here will be image
+                </figure>
+                <h4 className={styles.skill_heading}>Backend</h4>
+                <ul className={styles.skill_list}>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Node</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Express</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Nest</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>MongoDB</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Firebase</span>
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.skill}>
+                <figure className={styles.skill_image}>
+                  ...here will be image
+                </figure>
+                <h4 className={styles.skill_heading}>Mobile</h4>
+                <ul className={styles.skill_list}>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>React-Native</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Flutter</span>
+                  </li>
+                  <li className={styles.skill_list_item}>
+                    <span>circle</span>
+                    <span>Kotlin</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
-        
+        <footer className={styles.footer}>
+          <figure className={styles.footer_decoration_left}>
+            ...svg left top
+          </figure>
+          <figure className={styles.footer_decoration_right}>
+            ...svg right bottom
+          </figure>
+          <div className={styles.footer_top}>
+            <div className={styles.footer_top_left}>
+              <div>
+                <h4>Navigation</h4>
+                <div>
+                  <Link to={'/about'}>About active</Link>
+                  <Link to={'/about'}>Projects</Link>
+                </div>
+              </div>
+              <div>
+                <h4>Contact me</h4>
+                <div>
+                  <a href='mailto:sheludeshev.artyom@mail.ru'>@mail.me</a>
+                  <a href='t.me/x7evenUpx'>t.me/x7evenUpx</a>
+                </div>
+              </div>
+            </div>
+            <div className={styles.footer_top_right}>
+              <div>
+                <h4>I also write, sometimes</h4>
+                <p>About frontend dev, crypto trading, design and life</p>
+                <Button>
+                  <a href='https://ashel.vercel.app'>Read my blog</a>
+                </Button>
+              </div>
+              <div>
+                <h4>Make sure to see my works</h4>
+                <p>Open source, UI kits, UI libraries, websites and more</p>
+                <Button>
+                  <Link to={'/projects'}>See my works</Link>
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.footer_bottom}>
+            <span className={styles.copyright}>© Artyom Sheludeshev 2022</span>
+            <ul className={styles.socials}>
+              <li>VK</li>
+              <li>GH</li>
+              <li>IN</li>
+              <li>TW</li>
+            </ul>
+          </div>
+        </footer>
       </div>
     </>
   )
