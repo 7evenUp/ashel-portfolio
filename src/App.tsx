555c64a4484a3619e1d8c11411487ef4db69a543
Artyom <sheludeshev.artyom@mail.ru>
2022-08-18 13:18:16 +0300

    Started animating web page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.scss'
 import { Menu } from './components'
 import { Outlet } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -12,18 +13,31 @@ function App() {
 
   return (
     <div className="App">
-      {loading ? (
-        <h1>= = = Loading = = =</h1>
-      ) : (
-        <>
-          <h1 className='App_heading'>Ashel Portfolio</h1>
-          <div className='App_navigation_wrapper'>
-            <Menu />
-          </div>
-          
-          <Outlet />
-        </>
-      )}
+      <AnimatePresence>
+        {loading ? (
+            <motion.div
+              className='App_preloader'
+              key="preloader"
+              initial={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 2 }}
+            >
+              <h1>= = = Loading = = =</h1>
+            </motion.div>
+        ) : (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <h1 className='App_heading'>Ashel Portfolio</h1>
+            <div className='App_navigation_wrapper'>
+              <Menu />
+            </div>
+            
+            <Outlet />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
