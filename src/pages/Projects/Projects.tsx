e05760bd7deef287a17de34e17e78fbe568e4e65
Artyom <sheludeshev.artyom@mail.ru>
2022-08-19 14:19:24 +0300

    Small changes

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { SectionHeading } from '../../components'
+import { Footer, SectionHeading } from '../../components'
 import styles from './Projects.module.scss'
 
 export default function Projects() {
   return (
     <div className={styles.container}>
       <SectionHeading heading='My projects' />
+
+      <Footer />
     </div>
   )
 }
