57b4b94875fee5c7eee1a91b55a0c099267319cd
Artyom <sheludeshev.artyom@mail.ru>
2022-09-01 14:04:32 +0300

    Finished projects responsive styling

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -5,15 +5,23 @@ import styles from './Projects.module.scss'
 
 const Projects = () => {
   return (
-    <div className={styles.container}>
-      <SectionHeading heading='My projects' />
+    <>
+      <div className={styles.container}>
+        <SectionHeading heading='My projects' />
 
-      <div className={styles.projects}>
-        {projects.map((project, i) => <Project key={i} {...project} />)}
+        <div className={styles.projects}>
+          {projects.map((project, i) => <Project key={i} {...project} />)}
+        </div>
+
+        <div className={styles.computer_footer}>
+          <Footer />
+        </div>
       </div>
-
-      <Footer />
-    </div>
+      
+      <div className={styles.mobile_footer}>
+        <Footer />
+      </div>
+    </>
   )
 }
 
