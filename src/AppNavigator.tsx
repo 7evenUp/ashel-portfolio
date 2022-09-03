b48a1f89d77137ae6c2c6387311abfe2b025c81a
Artyom <sheludeshev.artyom@mail.ru>
2022-09-03 13:46:15 +0300

    Changes for build

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import { About, Projects } from './pages'
 
 const AppNavigator = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
           <Route path='/' element={<About />} />
           <Route path='/projects' element={<Projects />} />
         </Route>   
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
