fb032fb09ca0d6230c79a60199ad852997a5fd18
Artyom <sheludeshev.artyom@mail.ru>
2023-02-09 00:33:41 +0300

    Created ScrollToTop component for handling scroll restoration when navigating

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -1,18 +1,21 @@
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import App from './App'
-import { About, Projects } from './pages'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import { ScrollToTop } from "./components";
+import { About, Projects } from "./pages";
 
 const AppNavigator = () => {
   return (
     <HashRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route path='/' element={<About />} />
-          <Route path='/projects' element={<Projects />} />
-        </Route>   
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </HashRouter>
-  )
-}
+  );
+};
 
-export default AppNavigator
+export default AppNavigator;
